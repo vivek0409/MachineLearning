ce87abd663f2df4a79fc71f32ddfa84701b7f93e
VVVISHAL-JJ0GR4\Vivek Vishal <vivek>
2025-08-08 19:51:56 +0530

    removed unwanted imports

--- IntoToMachineLearning/CostFuntion.py
+++ IntoToMachineLearning/CostFuntion.py
@@ -1,6 +1,5 @@
 import matplotlib
 import numpy as np
-%matplotlib widget
 import matplotlib.pyplot as plt
 from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
 plt.style.use('./deeplearning.mplstyle')
