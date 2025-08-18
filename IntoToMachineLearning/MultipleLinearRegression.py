1b762d8ff1c81aea411501b9c9dace5a7f617d2f
VVVISHAL-JJ0GR4\Vivek Vishal <vivek>
2025-08-18 08:13:00 +0530

    MultipleLinearRegression.py

--- IntoToMachineLearning/MultipleLinearRegression.py
+++ IntoToMachineLearning/MultipleLinearRegression.py
@@ -17,32 +17,32 @@ b_init = 785.1811367994083
 w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
 print(f"w_init shape: {w_init.shape}, b_init type: {type(b_init)}")
 
-def pridict_single_loop(x,w,b):
-    n=x.shape[0]
-    p=0
-    for i in range(n):
-        p_i = x[i]+w[i]
-        p=p+p_i
-    p=p+b
-    return p
-
-x_vec = X_train[0,:]
-print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
-
-f_wb = pridict_single_loop(x_vec,w_init, b_init)
-print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
-
-def pridict (x,w,b):
-    p=np.dot(x,w)+b
-    return p
-
-x_vec = X_train[0,:]
-print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
-
-f_wb = pridict(x_vec,w_init, b_init)
-print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
-
-
+# def pridict_single_loop(x,w,b):
+#     n=x.shape[0]
+#     p=0
+#     for i in range(n):
+#         p_i = x[i]+w[i]
+#         p=p+p_i
+#     p=p+b
+#     return p
+#
+# x_vec = X_train[0,:]
+# print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+#
+# f_wb = pridict_single_loop(x_vec,w_init, b_init)
+# print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
+#
+# def pridict (x,w,b):
+#     p=np.dot(x,w)+b
+#     return p
+#
+# x_vec = X_train[0,:]
+# print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+#
+# f_wb = pridict(x_vec,w_init, b_init)
+# print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
+#
+#
 def compute_cost(X, y, w, b):
     """
     compute cost
