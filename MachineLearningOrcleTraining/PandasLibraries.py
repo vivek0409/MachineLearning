aaf70b9a42ed3bd66b0262e5eab7e5ed208aa45c
VVVISHAL-JJ0GR4\Vivek Vishal <vivek>
2025-08-18 18:35:20 +0530

    Python Training

--- MachineLearningOrcleTraining/PandasLibraries.py
+++ MachineLearningOrcleTraining/PandasLibraries.py
@@ -1,8 +1,26 @@
 import pandas as pd
 
 def main():
-    sample_data = pd.read_csv("../TestData/sample_data.csv")
-    print(sample_data)
+    sample_data = pd.read_csv("../TestData/Sample_data.csv")
+
+    sample_data['Total1'] = sample_data.loc[:,'Read':'SST'].sum(axis=1)
+    sample_data['Total2'] = sample_data.loc[:, 'Read':'SST'].sum(axis=0)
+
+    sample_data.drop('Total2', axis = 1, inplace = True)
+
+    sample_data.insert(11,"Percentage",sample_data.Total1/500*100)
+    # print(sample_data)
+    # print(sample_data[sample_data["Percentage"]>64])
+    print(sample_data.sort_values("Percentage",ascending=False))
+    
+
+    # sample_data.to_csv('../TestData/output.csv')
+
+    # print(sample_data.describe())
+    # print(sample_data.shape)
+    # print(sample_data.info)
+    # print(sample_data.loc[:,['Read','SST']])
+    # print(sample_data.head())
 
 
 if __name__== "__main__":
---
+++ MachineLearningOrcleTraining/SKLearnDataSet.py
@@ -0,0 +1,9 @@
+
+
+
+
+def main():
+    True
+
+if __name__== "__main__":
+    main()
