de4c27a3595f64b1f465d26f2151307f1a6b0860
VVVISHAL-JJ0GR4\Vivek Vishal <vivek>
2025-08-19 15:33:34 +0530

    Oracle Python Training Day 2

---
+++ MachineLearningOrcleTraining/dataVisulalisation.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv("../TestData/iris.csv")
+
+def main():
+    coRelation()
+
+def coRelation():
+    x  = np.random.randint(0,100,500)
+    y  = 100-x + np.random.normal(1,10,500)
+    z = np.random.randint(0, 100, 500)
+    print(np.corrcoef(x,z))
+    plt.scatter(x,z)
+    plt.show()
+
+def scatterPlot():
+    x = [1, 2, 3, 4, 5]
+    y = [5, 7, 4, 6, 8]
+    dataset = [x,y]
+    # sns.scatterplot(dataset)
+    plt.scatter(x,y)
+    plt.grid()
+    plt.show()
+
+def boxplotimplementation():
+    d1 = np.random.normal(100,50,1000)
+    d2 =  np.random.normal(-4,500,1000)
+    # print(data)
+    dataset = [d1,d2]
+
+    plt.boxplot(dataset)
+
+    plt.show()
+
+def histogram():
+    randon_int = np.random.randint(1,100, size = 100)
+    print(randon_int)
+
+    plt.hist(randon_int, bins = 10)
+    plt.show()
+
+def visualizeData():
+    print(dataset)
+
+    # sns.scatterplot(x='Sepal.Length' , y ='Sepal.Width' , hue = 'Species', data = dataset)
+    # plt.show()
+    # sns.countplot(y='Species',data = dataset)
+    # plt.show()
+    # subjects = ['maths', 'computers', 'english', 'Accounts', 'Economics']
+    # counts = [100, 40, 35, 29, 20]
+    #
+    # plt.title('Subject Count')
+    # plt.xlabel('Counts')
+    # plt.ylabel('Subjects')
+    # plt.barh(subjects,counts)
+    # plt.show()
+
+    data = [27, 45, 36, 18, 72, 81, 108, 19, 9]
+    x = range(len(data))
+
+    plt.title('Count')
+    plt.xlabel('Index')
+    plt.ylabel('Count')
+    plt.bar(x, data)
+    plt.show()
+
+def barchartImplementation():
+    fig, ax = plt.subplots()
+
+    fruits = ['apple', 'blueberry', 'cherry', 'orange']
+    counts = [40, 100, 30, 55]
+    bar_labels = ['red', 'blue', '_red', 'orange']
+    bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+    ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+    ax.set_ylabel('fruit supply')
+    ax.set_title('Fruit supply by kind and color')
+    ax.legend(title='Fruit color')
+
+    plt.show()
+
+def stackBarChart():
+    students = ['Minal', 'Mohit', 'Mukul', 'Pradeep']
+    Test1 = np.array([18, 14, 12, 15])
+    Test2 = np.array([20, 18, 16, 19])
+    Test3 = np.array([16, 19, 17, 18])
+    index = students
+    plt.bar(index, Test1, width=0.5, label='Test1', color='green', bottom=Test2 + Test3)
+    plt.bar(index, Test2, width=0.5, label='Test2', color='red', bottom=Test3)
+    plt.bar(index, Test3, width=0.5, label='Test3', color='blue')
+    plt.ylabel("Marks")
+    plt.xlabel("Students")
+    plt.legend(loc='upper right')
+    plt.title("Marks obtained in three subjects")
+    plt.show()
+
+if __name__== "__main__":
+    main()
---
+++ MachineLearningOrcleTraining/uciRepo.py
@@ -0,0 +1,105 @@
+import sqlite3
+from venv import create
+
+import pandas as pd
+from ucimlrepo import fetch_ucirepo
+import numpy as np
+from sklearn import preprocessing
+import xlsxwriter
+import sqlite3 as sql
+
+
+def studentPerformance():
+    # fetch dataset
+    student_performance = fetch_ucirepo(id=320)
+
+    # data (as pandas dataframes)
+    X = student_performance.data.features
+    y = student_performance.data.targets
+
+    # metadata
+    # print(student_performance.metadata)
+
+    # variable information
+    # print(student_performance.variables)
+
+    # print(student_performance.data.shape)
+
+    df_president_name = pd.DataFrame({'first': ['George', 'Bill', 'Ronals', 'Jimmy', 'George'],
+                                      'last': ['Bush', 'Clinton', 'Regan', 'Carter', 'Washington']})
+    # print(df_president_name)
+    grouped = df_president_name.groupby('first')
+    # print(grouped.get_group('George'))
+
+    df_test_scores = pd.DataFrame({'test1': [95, 84, 73, 88, 82, 61],
+                                   'test2': [74, 85, 82, 73, 77, 79]},
+                                  index=['jack', 'Lewis', 'patrick', 'rich', 'kelly', 'paula'])
+    print(df_test_scores)
+    print(standardize_tests(df_test_scores))
+    print(standardize_tests_score(df_test_scores))
+
+def standardize_tests(test):
+    return(test-test.mean())/test.std()
+
+
+def standardize_tests_score(test):
+    return test.apply(standardize_tests)
+
+def dataLabeling():
+    input_labels = ['justice', 'patience', 'justice', 'truth', 'patience', 'loyalty', 'perseverance']
+    encoder = preprocessing.LabelEncoder()
+    encoder.fit(input_labels)
+    test_labels = ['truth', 'justice', 'patience', 'loyalty']
+    encoded_values = encoder.transform(test_labels)
+    print(test_labels)
+    encoded_values = [2, 0, 4, 3]
+    decoded_list = encoder.inverse_transform(encoded_values)
+    print(encoded_values)
+    print(list(decoded_list))
+
+def dataFrameSample():
+    d = {'s1': [100, 90, np.nan, 95], 's2': [30, 45, 56, np.nan], 's3': [np.nan, 40, 50, 60]}
+    df = pd.DataFrame(d)
+    print(df)
+    print(df.isnull())
+    print(df.fillna('BAC'))
+
+def excelReaderAndWriter():
+    wb = xlsxwriter.Workbook('../TestData/mybook.xlsx')
+    worksheet = wb.add_worksheet()
+    chart = wb.add_chart({'type': 'line'})
+
+    data = [
+        [10, 20, 30, 40, 50],
+        [20, 40, 60, 80, 100],
+        [30, 60, 90, 120, 150],
+    ]
+    worksheet.write_column('A1', data[0])
+    worksheet.write_column('B1', data[1])
+    worksheet.write_column('C1', data[2])
+
+    chart.add_series({'values': '=Sheet1!$A$1:$A$5'})
+    chart.add_series({'values': '=Sheet1!$B$1:$B$5'})
+    chart.add_series({'values': '=Sheet1!$C$1:$C$5'})
+
+    worksheet.insert_chart('B7', chart)
+
+    wb.close()
+
+def sqlLiteImplementaion():
+    create_table = """ create table student_score 
+    (id INTEGER,name VARCHAR(20),math REAL,science REAL);"""
+    executeSQL = sqlite3.connect(':memory:')
+    executeSQL.execute(create_table)
+    insertSQL = [(10, 'lakshmi', 89, 92), (20, 'padmasree', 89, 98), (30, 'sree', 78, 98)]
+    insert_statement = "Insert into student_score values(?,?,?,?)"
+    executeSQL.executemany(insert_statement, insertSQL)
+    executeSQL.commit()
+    df_student_rec = pd.DataFrame(executeSQL.execute('select * from student_score').fetchall())
+    print(df_student_rec)
+
+def main():
+    sqlLiteImplementaion()
+
+if __name__== "__main__":
+    main()
