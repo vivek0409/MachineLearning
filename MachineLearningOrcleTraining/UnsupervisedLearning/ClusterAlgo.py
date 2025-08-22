8a52d5a56143aadaee6375ee7c9cfa4381c84593
VVVISHAL-JJ0GR4\Vivek Vishal <vivek>
2025-08-22 09:53:27 +0530

    Oracle Python Training Day 5

---
+++ MachineLearningOrcleTraining/UnsupervisedLearning/ClusterAlgo.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import matplotlib
+from apyori import apriori
+
+
+
+def main():
+    clusterAlgo_Malldata()
+
+
+def clusterAlgo_Malldata():
+    df = pd.read_csv("../../TestData/Mall_Customers.csv")
+    print(df)
+
+
+if __name__== "__main__":
+    main()
